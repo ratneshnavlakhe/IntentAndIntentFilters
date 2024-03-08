cf9d8cd580e43a982756820f84c6b542eb2dc9b5
Ratnesh Navlakhe <ratnesh061288@gmail.com>
2024-03-08 18:57:19 +0530

    Example of opening youtube app using explicit intent

--- app/src/main/java/com/example/intentandintentfilters/MainActivity.kt
+++ app/src/main/java/com/example/intentandintentfilters/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.intentandintentfilters
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.intentandintentfilters.ui.theme.IntentAndIntentFiltersTheme
@@ -17,12 +21,23 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             IntentAndIntentFiltersTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Greeting("Android")
+                    Button(onClick = {
+                        Intent(Intent.ACTION_MAIN).also {
+                            it.`package` = "com.google.android.youtube"
+                            try {
+                                startActivity(it)
+                            } catch (e: ActivityNotFoundException) {
+                                e.printStackTrace()
+                            }
+                        }
+                    }) {
+                        Text(text = "Click me")
+                    }
                 }
             }
         }
