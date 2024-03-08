82f81c30713a1113f1687ca1186b9cc018728732
Ratnesh Navlakhe <ratnesh061288@gmail.com>
2024-03-08 19:44:01 +0530

    Example of implicit intent via sending an email template from the app to an email client

--- app/src/main/java/com/example/intentandintentfilters/MainActivity.kt
+++ app/src/main/java/com/example/intentandintentfilters/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.intentandintentfilters
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.intentandintentfilters.ui.theme.IntentAndIntentFiltersTheme
@@ -17,12 +20,24 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             IntentAndIntentFiltersTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Greeting("Android")
+                    Button(onClick = {
+                        val intent = Intent(Intent.ACTION_SEND).apply {
+                            type = "text/plain"
+                            putExtra(Intent.EXTRA_EMAIL, arrayOf("test@test.com"))
+                            putExtra(Intent.EXTRA_SUBJECT, "this is my subject")
+                            putExtra(Intent.EXTRA_TEXT, "This is the content of my email")
+                        }
+                        if (intent.resolveActivity(packageManager) != null) {
+                            startActivity(intent)
+                        }
+                    }) {
+                        Text(text = "Click me!")
+                    }
                 }
             }
         }
